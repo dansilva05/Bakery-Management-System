32ae3f4473c95bd7d9092232d38ff72140702cfa
silva-danilo05 <20112943@setu.ie>
2026-04-15 13:01:18 +0100

    Add SearchResult model

---
+++ src/main/java/ie/setu/bakeryca/models/SearchResult.java
@@ -0,0 +1,35 @@
+package ie.setu.bakeryca.models;
+
+// asked Claude for help
+public class SearchResult {
+
+    private String type;
+    private BakedGood bakedGood; // populated when kind is "BakedGood"
+    private Ingredient ingredient;
+
+    // builds a result for a baked good match
+    public SearchResult(BakedGood bakedGood) {
+        this.type = "BakedGood";
+        this.bakedGood = bakedGood;
+    }
+
+    // builds a result for an ingredient match
+    public SearchResult(Ingredient ingredient) {
+        this.type = "Ingredient";
+        this.ingredient = ingredient;
+    }
+
+    public String getType() { return type; }
+    public BakedGood getBakedGood() { return bakedGood; }
+    public Ingredient getIngredient() { return ingredient; }
+
+    // shown in the search results list
+    @Override
+    public String toString() {
+        if (type.equals("BakedGood")) {
+            return "[Baked Good] " + bakedGood.toString();
+        } else {
+            return "[Ingredient] " + ingredient.toString();
+        }
+    }
+}
