364dad0107e6a3e23b081f0f18ac8136906d1f1c
silva-danilo05 <20112943@setu.ie>
2026-04-27 23:06:17 +0100

    Implement sort search result 'by name' and 'by calories'

--- src/main/java/ie/setu/bakeryca/core/Sort.java
+++ src/main/java/ie/setu/bakeryca/core/Sort.java
@@ -1,7 +1,6 @@
 package ie.setu.bakeryca.core;
 
-import ie.setu.bakeryca.models.BakedGood;
-import ie.setu.bakeryca.models.Ingredient;
+import ie.setu.bakeryca.models.*;
 
 public class Sort {
 
@@ -54,6 +53,50 @@ public class Sort {
                 list.set(i, list.get(i - 1));
             }
             list.set(i, elem);
+        }
+    }
+
+    // sorts mixed search results alphabetically by name
+    public static void sortSearchResultsByName(LinkedList<SearchResult> list) {
+        for (int e = 1; e < list.size(); e++) {
+            SearchResult elem = list.get(e);
+            int i;
+            for (i = e; i >= 1 && nameOf(list.get(i - 1)).compareToIgnoreCase(nameOf(elem)) > 0; i--) {
+                list.set(i, list.get(i - 1));
+            }
+            list.set(i, elem);
+        }
+    }
+
+    // sorts mixed search results by calories, lowest first
+    // (baked goods use total recipe calories, ingredients use kcal/100g)
+    public static void sortSearchResultsByCalories(LinkedList<SearchResult> list) {
+        for (int e = 1; e < list.size(); e++) {
+            SearchResult elem = list.get(e);
+            double elemCals = caloriesOf(elem);
+            int i;
+            for (i = e; i >= 1 && caloriesOf(list.get(i - 1)) > elemCals; i--) {
+                list.set(i, list.get(i - 1));
+            }
+            list.set(i, elem);
+        }
+    }
+
+    // helper - pulls the name out of either kind of search result
+    private static String nameOf(SearchResult sr) {
+        if (sr.getType().equals("BakedGood")) {
+            return sr.getBakedGood().getName();
+        } else {
+            return sr.getIngredient().getName();
+        }
+    }
+
+    // helper - pulls the calorie value out of either kind of search result
+    private static double caloriesOf(SearchResult sr) {
+        if (sr.getType().equals("BakedGood")) {
+            return sr.getBakedGood().getRecipe().getTotalCalories();
+        } else {
+            return sr.getIngredient().getCaloriesPer100g();
         }
     }
 }
